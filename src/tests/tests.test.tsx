91e8a08451a2345b73a78e9d2c4191d12ac17596
Vitali Savelyev <vsavelyev@elinext.com>
2023-04-17 14:48:19 +0300

    fix: fix tests

--- src/tests/tests.test.tsx
+++ src/tests/tests.test.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Routes } from 'react-router-dom';
-import About from '../routes/about/about';
-import Form from '../routes/forms/forms';
+import { BrowserRouter } from 'react-router-dom';
 import { beforeEach, describe, expect, test } from 'vitest';
-import Home from '../routes/home/home';
-import NotFound from '../routes/notfound/notFound';
 import { render, screen } from '@testing-library/react';
-import { Route } from 'react-router';
-import Header from '../header/header';
+import {Provider} from "react-redux";
+import {store} from "../store";
+import App from "../app";
 
 describe('Testing routing', () => {
   beforeEach(() => {
     render(
-      <React.StrictMode>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/form" element={<Form />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </React.StrictMode>
+        <React.StrictMode>
+          <BrowserRouter>
+            <Provider store={store}>
+              <App />
+            </Provider>
+          </BrowserRouter>
+        </React.StrictMode>
     );
   });
 
